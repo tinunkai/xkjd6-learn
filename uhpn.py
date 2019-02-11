e8b19a88202f332707d93ace54fd50c31225ac64
Tin Unkai <tinunkai@gmail.com>
2019-02-11 15:47:30 +0900

    init

---
+++ uhpn.py
@@ -0,0 +1,36 @@
+table = {
+    'iu': Q,
+    'ia': W,
+    'ua': W,
+    'uan': R,
+    'ue': T,
+    've': T,
+    'ing': Y,
+    'uai': Y,
+    'sh': U,
+    'ch': I,
+    'uo': O,
+    'un': P,
+    'iong': S,
+    'ong': S,
+    'iang': D,
+    'uang': D,
+    'en': F,
+    'eng': G,
+    'ang': H,
+    'an': J,
+    'ao': K,
+    'ai': L,
+    'ei': Z,
+    'ie': X,
+    'iao': C,
+    'zh': V,
+    'ui': V,
+    'ou': B,
+    'in': N,
+    'ian': M,
+}
+
+import random
+
+print(random.choice(table))
